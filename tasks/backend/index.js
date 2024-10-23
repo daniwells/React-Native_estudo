a6a048fc12a2512f233d0444ac4969231d33f526
daniwells <daniellimafreita@gmail.com>
2024-10-22 22:05:30 -0300

    feat: desenvolvendo backend da aplicação tasks

--- tasks/backend/index.js
+++ tasks/backend/index.js
@@ -1,7 +1,21 @@
 const bodyParser = require("body-parser");
 const express = require("express")
 const app = express()
+const db = require("./config/db")
+const consign = require("consign")
 
+consign()
+    .include("./config/passport.js")
+    .then("./config/middlewares.js")
+    .then("./api")
+    .then("./config/routes.js")
+    .into(app)
+
+console.log("Modules loaded...");
+
+app.db = db;
+
+console.log("Db loaded...");
 
 // app.use(myJson());
 // app.use(bodyParser.json());
